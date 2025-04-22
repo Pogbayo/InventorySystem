986815dc5e339812974e095000a5ec71ce1f96ad
Pogbayoe <adebayooluwasegun335@gmail.com>
2025-04-22 20:56:49 +0100

    Nullable field

--- InventorySystem.Application/DTOs/ApllicationUserDto/UserGetDto.cs
+++ InventorySystem.Application/DTOs/ApllicationUserDto/UserGetDto.cs
@@ -5,7 +5,7 @@ namespace InventorySystem.Application.DTOs.ApllicationUserDto
     public class UserGetDto
     {
         public required string Id { get; set; }
-        public required string Email { get; set; }
+        public string? Email { get; set; }
         public IList<string> Roles { get; set; } = new List<string>();
     }
 }
